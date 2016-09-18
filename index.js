f1c4a74875d808753db84d024943f52d9860af8b
楼教主 <cik520@qq.com>
2016-09-18 22:49:11 +0800

    fix bug

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ module.exports = function(prefix) {
     if (url.indexOf(prefix) !== 0) {
       yield next;
     } else {
+      this.status = 200;
+      this.respond = false;
       placehold({url: url.replace(prefix, '')}, this.res);
     }
   };
